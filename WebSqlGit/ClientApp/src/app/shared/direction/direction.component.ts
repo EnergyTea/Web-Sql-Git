32abbc8fe6386239d46dd26e8d8e2f14bd92188d
Руслан Васильев <rusvas15@yandex.ru>
2020-07-16 13:52:17 +0300

    fix

--- WebSqlGit/ClientApp/src/app/shared/direction/direction.component.ts
+++ WebSqlGit/ClientApp/src/app/shared/direction/direction.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'direction',
-    templateUrl: './direction.component.html'
+    templateUrl: './direction.component.html',
+    styleUrls: ['./direction.component.css']
 })
 /** direction component*/
 export class DirectionComponent {
