ae8e4bb06b5db24b698006ac8bd3ffa019cbbf73
igor.kadkalo <igor.kadkalo@travelline.ru>
2020-10-21 12:20:24 +0300

    Добавил хранение паролей в шифрованном виде, обновил файл gitignore

---
+++ WebSqlGit/Helpes/HashHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace WebSqlGit.Helpes
+{
+    public static class HashHelper
+    {
+        public static string TextToHash( string text )
+        {
+            SHA1 sh = SHA1.Create();
+            StringBuilder hash = new StringBuilder();
+            byte[] bytes = Encoding.UTF8.GetBytes( text );
+            byte[] b = sh.ComputeHash( bytes );
+            foreach ( byte a in b )
+            {
+                var h = a.ToString( "x2" );
+                hash.Append( h );
+            }
+
+            return hash.ToString();
+        }
+    }
+}
--- WebSqlGit/Startup.cs
+++ WebSqlGit/Startup.cs
@@ -26,9 +26,6 @@ namespace WebSqlGit
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices( IServiceCollection services )
         {
-
-
-
             ConfigSettings configSettings = new ConfigSettings();
             DbContextConfiguration dbContextConfiguration = configSettings.DbContextConfiguration;
 
